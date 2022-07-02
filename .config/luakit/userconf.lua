fa5bfed494b47479db521d27e0b223a9ad807e7d
izder456 <izder456@protonmail.org>
2022-07-01 23:05:05 -0500

    fix themes n stuff

--- .config/luakit/userconf.lua
+++ .config/luakit/userconf.lua
@@ -1,8 +1,10 @@
 local settings = require "settings"
 settings.window.home_page = "68k.news"
 settings.window.search_engines = {
+	searx = "https://search.disroot.org/search?q=%s";
 	whoogle = "https://whoogle.herokuapp.com/search?q=%s";
 	ddg = "https://html.duckduckgo.com/html/?q=%s";
-	searx = "https://swag.pw/search?q=%s"
+	frog = "http://frogfind.com/?=%s";
+	brave = "https://search.brave.com=/search?=%s"
 }
 settings.window.default_search_engine = "whoogle"
--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,8 +1,9 @@
 config.load_autoconfig()
 c.url.start_pages = ["http://68k.news/"]
 c.url.searchengines = {
-    "DEFAULT": "https://swag.pw/search?q={}",
+   	"DEFAULT": "https://search.disroot.org/search?q={}",
 	"whoogle": "https://whoogle.herokuapp.com/search?q={}", 
 	"ddg": "https://html.duckduckgo.com/html/?q={}", 
-	"frog": "http://frogfind.com/?q={}"
+	"frog": "http://frogfind.com/?q={}",
+	"brave": "https://search.brave.com/search?q={}" 
 }
