cbf0a79ba62818aced1da2a5cfcb43909253c5ef
izder456 <izder456@protonmail.com>
2022-08-30 11:56:27 -0500

    update

--- .config/luakit/userconf.lua
+++ .config/luakit/userconf.lua
@@ -1,7 +1,7 @@
 local settings = require "settings"
 settings.window.home_page = "68k.news"
 settings.window.search_engines = {
-	searx = "https://search.disroot.org/search?q=%s";
+	searx = "https://searx.org/search?q=%s";
 	whoogle = "https://whoogle.herokuapp.com/search?q=%s";
 	ddg = "https://html.duckduckgo.com/html/?q=%s";
 	frog = "http://frogfind.com/?=%s";
--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,7 +1,7 @@
 config.load_autoconfig()
 c.url.start_pages = ["http://68k.news/"]
 c.url.searchengines = {
-   	"DEFAULT": "https://search.disroot.org/search?q={}",
+   	"DEFAULT": "https://searx.org/search?q={}",
 	"whoogle": "https://whoogle.herokuapp.com/search?q={}", 
 	"ddg": "https://html.duckduckgo.com/html/?q={}", 
 	"frog": "http://frogfind.com/?q={}",
