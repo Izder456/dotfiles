521c6b322cd7993634495da62e6230a1e152a8e8
izzy <izder456@protonmail.com>
2022-12-15 16:48:16 -0600

    cleanup

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,8 +1,8 @@
 config.load_autoconfig()
 c.url.start_pages = ["http://68k.news/"]
 c.url.searchengines = {
-   	"DEFAULT": "https://searx.org/search?q={}",
-	"whoogle": "https://whoogle.herokuapp.com/search?q={}", 
+	"DEFAULT": "https://www.whoogle.click/search?q={}", 
+   	"searx": "https://priv.au/search?q={}",
 	"ddg": "https://html.duckduckgo.com/html/?q={}", 
 	"frog": "http://frogfind.com/?q={}",
 	"brave": "https://search.brave.com/search?q={}" 
