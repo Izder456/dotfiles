a9411f47b226c3934c4d694e5aad9b9af3aeab8c
izzY <izder456@protonmail.com>
2023-05-26 02:49:35 -0500

    add test refactor 3

--- setup.pl
+++ setup.pl
@@ -3,76 +3,71 @@
 use strict;
 use warnings;
 
-print "Welcome to iz's OpenBSD setup Perl script!\n";
-print "This assumes you have installed the doas.conf file in the root of this project.\n";
-print "Checking if doas.conf is installed...\n";
+# Prints a message with a line break
+sub print_message {
+    my ($message) = @_;
+    print "$message\n";
+}
 
-my $destination = "/etc/doas.conf"; # Destination file path
+# Checks if a file exists
+sub file_exists {
+    my ($filepath) = @_;
+    return -e $filepath;
+}
 
-if (-e $destination) {
-    print "The destination file $destination already exists.\n";
-} else {
-    print "The destination file $destination does not exist.\n";
-
-    my $url = "https://github.com/izder456/dotfiles/raw/main/doas.conf"; # URL of the doas.conf file
-    my $filename = "doas.conf"; # File name for saving the downloaded file
-
-    print "Downloading the file to the current directory...\n";
-
-    # Download the file using ftp command
+# Downloads a file from a given URL
+sub download_file {
+    my ($url, $filename) = @_;
     my $status = system("ftp -o $filename $url");
+    return $status == 0;
+}
 
-    if ($status == 0) {
-        print "File downloaded successfully!\n";
-        print "Please copy the file $filename to $destination manually.\n";
+# Removes default OpenBSD files
+sub remove_default_files {
+    unlink(
+        "$ENV{HOME}/.cshrc",
+        "$ENV{HOME}/.login",
+        "$ENV{HOME}/.mailrc",
+        "$ENV{HOME}/.profile",
+        "$ENV{HOME}/.Xdefaults",
+        "$ENV{HOME}/.cvsrc"
+    );
+}
+
+# Sets permissions for user's HOME directory
+sub set_home_directory_perms {
+    chmod(0700, $ENV{'HOME'});
+}
+
+# Installs dependencies using package manager
+sub install_dependencies {
+    my @dependencies = @_;
+    system('doas', 'pkg_add', @dependencies);
+}
+
+# Updates dotfiles repository or clones if not present
+sub update_or_clone_dotfiles {
+    if (-d "$ENV{HOME}/.dotfiles") {
+        chdir "$ENV{HOME}/.dotfiles";
+        system('git', 'pull', '--ff-only');
     } else {
-        print "Failed to download the file. Please check your internet connection.\n";
+        system('git', 'clone', 'https://github.com/izder456/dotfiles', "$ENV{HOME}/.dotfiles");
     }
-    exit();
 }
 
-# remove cruft installed by default in openbsd
-print "Removing OpenBSD default Cruft...\n";
-unlink(
-    "$ENV{HOME}/.cshrc",
-    "$ENV{HOME}/.login",
-    "$ENV{HOME}/.mailrc",
-    "$ENV{HOME}/.profile",
-    "$ENV{HOME}/.Xdefaults",
-    "$ENV{HOME}/.cvsrc"
-);
-
-print "Setting 0700 Perms for user's HOME...\n";
-chmod(0700, $ENV{'HOME'});
-
-print "Installing deps...\n";
-our @shdeps = ('zsh', 'bash', 'harfbuzz', 'neofetch', 'git', 'gmake', 'gawk', 'cmake', 'meson', 'upower', 'gcc', 'mercurial', 'feh', 'ffmpeg', 'yt-dlp', 'ImageMagick', 'gd', 'fftw3', 'fftw', 'automake', 'autoconf', 'neovim', 'dbus', 'htop', 'ncspot', 'rust', 'crystal', 'exa', 'pkg_mgr', 'scrot', 'py3-neovim', 'py3-pip', 'lynx', 'links', 'wget', 'curl', 'openssl','gmp', 'xz-utils', 'p7zip', 'bat', 'pkgconf', 'noto-emoji', 'ranger', 'ee', 'nano');
-our @xdeps = ('sdorfehs', 'xdg-user-dirs', 'xdg-utils', 'gtk2-murrine-engine', 'mpv', 'qutebrowser', 'abiword', 'gnumeric', 'pcmanfm', 'weechat', 'dunst', 'picom', 'rofi', 'leafpad', 'xarchiver', 'xkill', 'xedit', 'xpdf', 'lxappearance', 'claws-mail');
-system('doas', 'pkg_add', @shdeps, @xdeps);
-
-if (-d "$ENV{HOME}/.dotfiles") {
-    chdir "$ENV{HOME}/.dotfiles";
-    system('git', 'pull', '--ff-only');
-} else {
-    system('git', 'clone', 'https://github.com/izder456/dotfiles', "$ENV{HOME}/.dotfiles");
+# Symlinks dotfiles from .dotfiles directory to home directory
+sub symlink_dotfiles {
+    system('ln', '-sf', glob("$ENV{HOME}/.dotfiles/.*"), '.');
 }
 
-opendir(my $dh, $ENV{'HOME'}) or die "Cannot open directory: $!";
-
-print "Symlinking Dotfiles...\n";
-system('ln', '-sf', glob("$ENV{HOME}/.dotfiles/.*"), '.');
-
-print "Setting up default shell...\n";
-if (-d "$ENV{HOME}/.fizsh") {
-    chdir "$ENV{HOME}/.fizsh";
-    system('./configure');
-    system('make');
-    system('doas', 'make', 'install');
-    system('cp', "$ENV{HOME}/.dotfiles/.fizshrc", "$ENV{HOME}/.fizsh/.fizshrc");
-    system('chsh', '-s', '/usr/local/bin/fizsh');
-} else {
-    system('git', 'clone', 'https://github.com/zsh-users/fizsh.git', "$ENV{HOME}/.fizsh");
-    chdir "$ENV{HOME}/.fizsh";
+# Configures and sets up the default shell
+sub configure_default_shell {
+    if (-d "$ENV{HOME}/.fizsh") {
+        chdir "$ENV{HOME}/.fizsh";
+    } else {
+        system('git', 'clone', 'https://github.com/zsh-users/fizsh.git', "$ENV{HOME}/.fizsh");
+        chdir "$ENV{HOME}/.fizsh";
+    }
     system('./configure');
     system('make');
     system('doas', 'make', 'install');
@@ -80,40 +75,131 @@ if (-d "$ENV{HOME}/.fizsh") {
     system('chsh', '-s', '/usr/local/bin/fizsh');
 }
 
-print "Installing backgrounds...\n";
-system('doas', 'mkdir', '-p', '/usr/local/share/backgrounds');
-system('doas', 'cp', '-rvf', glob("$ENV{HOME}/.dotfiles/backgrounds/*"), '/usr/local/share/backgrounds');
+# Installs backgrounds to /usr/local/share/backgrounds
+sub install_backgrounds {
+    system('doas', 'mkdir', '-p', '/usr/local/share/backgrounds');
+    system('doas', 'cp', '-rvf', glob("$ENV{HOME}/.dotfiles/backgrounds/*"), '/usr/local/share/backgrounds');
+}
 
-print "Setting up Xenodm...\n";
-system('doas', 'ln', '-sf', glob("$ENV{HOME}/.dotfiles/xenodm_config/*"), '/etc/X11/xenodm/');
+# Sets up Xenodm configuration
+sub setup_xenodm {
+    system('doas', 'ln', '-sf', glob("$ENV{HOME}/.dotfiles/xenodm_config/*"), '/etc/X11/xenodm/');
+}
 
-print "Installing Fonts...\n";
-system('doas', 'cp', '-rvf', glob("$ENV{HOME}/.dotfiles/.fonts/*"), '/usr/X11R6/lib/X11/fonts/TTF/');
-system('doas', 'fc-cache', '-fv');
+# Installs fonts to /usr/X11R6/lib/X11/fonts/TTF
+sub install_fonts {
+    system('doas', 'cp', '-rvf', glob("$ENV{HOME}/.dotfiles/.fonts/*"), '/usr/X11R6/lib/X11/fonts/TTF/');
+    system('doas', 'fc-cache', '-fv');
+}
 
-print "Compiling in rust programs...( this is gonna take a bit :3 )\n";
-our @rsdeps = ('fd-find', 'sd', 'onefetch', 'tokei', 'zoxide', 'broot', 'du-dust', 'cargo-update-installed');
-system('cargo', 'install', @rsdeps);
+# Compiles rust programs using Cargo
+sub compile_rust_programs {
+    my @rust_dependencies = ('fd-find', 'sd', 'onefetch', 'tokei', 'zoxide', 'broot', 'du-dust', 'cargo-update-installed');
+    system('cargo', 'install', @rust_dependencies);
+}
 
-print "Compiling in GNU shuf re-implementation...\n";
-system('git', 'clone', 'https://github.com/ibara/shuf.git', "$ENV{HOME}/.shuf");
-chdir "$ENV{HOME}/.shuf";
-system('./configure');
-system('make');
-system('doas', 'make', 'install');
+# Compiles shuf re-implementation
+sub compile_shuf {
+    system('git', 'clone', 'https://github.com/ibara/shuf.git', "$ENV{HOME}/.shuf");
+    chdir "$ENV{HOME}/.shuf";
+    system('./configure');
+    system('make');
+    system('doas', 'make', 'install');
+}
 
-print "Compiling in suckless-term...\n";
-system('git', 'clone', 'https://github.com/izder456/st.git', "$ENV{HOME}/.st");
-chdir "$ENV{HOME}/.st";
-system('make');
-system('doas', 'make', 'install');
+# Compiles suckless-term (st)
+sub compile_suckless_term {
+    system('git', 'clone', 'https://github.com/izder456/st.git', "$ENV{HOME}/.st");
+    chdir "$ENV{HOME}/.st";
+    system('make');
+    system('doas', 'make', 'install');
+}
 
-print "Compiling in afetch...\n";
-system('git', 'clone', 'https://github.com/13-CF/afetch.git', "$ENV{HOME}/.afetch");
-chdir "$ENV{HOME}/.afetch";
-system('make');
-system('doas', 'make', 'install');
+# Compiles afetch
+sub compile_afetch {
+    system('git', 'clone', 'https://github.com/13-CF/afetch.git', "$ENV{HOME}/.afetch");
+    chdir "$ENV{HOME}/.afetch";
+    system('make');
+    system('doas', 'make', 'install');
+}
 
-closedir $dh;
+# Updates XDG user directories
+sub update_xdg_user_dirs {
+    system('xdg-user-dirs-update');
+}
 
-system('xdg-user-dirs-update');
+# Main script
+sub main {
+    print_message("Welcome to iz's OpenBSD setup Perl script!");
+    print_message("This assumes you have installed the doas.conf file in the root of this project.");
+    print_message("Checking if doas.conf is installed...");
+
+    my $destination = "/etc/doas.conf";
+
+    if (file_exists($destination)) {
+        print_message("The destination file $destination already exists.");
+    } else {
+        print_message("The destination file $destination does not exist.");
+
+        my $url = "https://github.com/izder456/dotfiles/raw/main/doas.conf";
+        my $filename = "doas.conf";
+
+        print_message("Downloading the file to the current directory...");
+
+        if (download_file($url, $filename)) {
+            print_message("File downloaded successfully!");
+            print_message("Please copy the file $filename to $destination manually.");
+        } else {
+            print_message("Failed to download the file. Please check your internet connection.");
+        }
+        exit();
+    }
+
+    print_message("Removing OpenBSD default cruft...");
+    remove_default_files();
+
+    print_message("Setting 0700 permissions for user's HOME...");
+    set_home_directory_perms();
+
+    print_message("Installing dependencies...");
+    my @shell_dependencies = ('zsh', 'bash', 'harfbuzz', 'neofetch', 'git', 'iftop', 'gmake', 'gawk', 'cmake', 'meson', 'upower', 'gcc', 'mercurial', 'feh', 'ffmpeg', 'yt-dlp', 'ImageMagick', 'gd', 'fftw3', 'fftw', 'automake', 'autoconf', 'neovim', 'dbus', 'htop', 'ncspot', 'rust', 'crystal', 'exa', 'pkg_mgr', 'scrot', 'py3-neovim', 'py3-pip', 'lynx', 'links', 'wget', 'curl', 'openssl', 'gmp', 'p7zip', 'bat', 'pkgconf', 'noto-emoji', 'ranger', 'ee', 'nano');
+    my @xdeps = ('sdorfehs', 'xdg-user-dirs', 'xdg-utils', 'gtk2-murrine-engine', 'mpv', 'qutebrowser', 'abiword', 'gnumeric', 'pcmanfm', 'weechat', 'dunst', 'picom', 'rofi', 'leafpad', 'xarchiver', 'xrandr', 'xpdf', 'lxappearance', 'claws-mail');
+    install_dependencies(@shell_dependencies, @xdeps);
+
+    update_or_clone_dotfiles();
+
+    opendir(my $dh, $ENV{'HOME'}) or die "Cannot open directory: $!";
+    print_message("Symlinking dotfiles...");
+    symlink_dotfiles();
+
+    print_message("Setting up default shell...");
+    configure_default_shell();
+
+    print_message("Installing backgrounds...");
+    install_backgrounds();
+
+    print_message("Setting up Xenodm...");
+    setup_xenodm();
+
+    print_message("Installing fonts...");
+    install_fonts();
+
+    print_message("Compiling Rust programs (this may take a while)...");
+    compile_rust_programs();
+
+    print_message("Compiling GNU shuf re-implementation...");
+    compile_shuf();
+
+    print_message("Compiling suckless-term...");
+    compile_suckless_term();
+
+    print_message("Compiling afetch...");
+    compile_afetch();
+
+    closedir $dh;
+
+    update_xdg_user_dirs();
+}
+
+# Call the main subroutine
+main();
