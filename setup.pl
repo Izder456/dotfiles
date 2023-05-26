3a36662998114e0aa1a9e6f29518482445363cfa
izzY <izder456@protonmail.com>
2023-05-26 02:19:11 -0500

    fix

--- setup.pl
+++ setup.pl
@@ -33,13 +33,13 @@ if (-e $destination) {
 
 # remove cruft installed by default in openbsd
 print "Removing OpenBSD default Cruft...\n";
-unlink (
-    "$ENV{'HOME'}/.cshrc",
-    "$ENV{'HOME'}/.login",
-    "$ENV{'HOME'}/.mailrc",
-    "$ENV{'HOME'}/.profile",
-    "$ENV{'HOME'}/.Xdefaults",
-    "$ENV{'HOME'}/.cvsrc"
+unlink(
+    "$ENV{HOME}/.cshrc",
+    "$ENV{HOME}/.login",
+    "$ENV{HOME}/.mailrc",
+    "$ENV{HOME}/.profile",
+    "$ENV{HOME}/.Xdefaults",
+    "$ENV{HOME}/.cvsrc"
 );
 
 print "Setting 0700 Perms for user's HOME...\n";
@@ -50,78 +50,69 @@ our @shdeps = ('zsh', 'bash', 'harfbuzz', 'neofetch', 'git', 'gmake', 'gawk', 'c
 our @xdeps = ('sdorfehs', 'xdg-user-dirs', 'xdg-utils', 'gtk2-murrine-engine', 'mpv', 'qutebrowser', 'abiword', 'gnumeric', 'pcmanfm', 'weechat', 'dunst', 'picom', 'rofi', 'leafpad', 'xarchiver', 'xkill', 'xedit', 'xpdf', 'lxappearance', 'claws-mail');
 system('doas', 'pkg_add', @shdeps, @xdeps);
 
-if (-d "$ENV{'HOME'}/.dotfiles") {
-    chdir "$ENV{'HOME'}/.dotfiles";
+if (-d "$ENV{HOME}/.dotfiles") {
+    chdir "$ENV{HOME}/.dotfiles";
     system('git', 'pull', '--ff-only');
 } else {
-    system('git', 'clone', 'https://github.com/izder456/dotfiles', "$ENV{'HOME'}/.dotfiles");
+    system('git', 'clone', 'https://github.com/izder456/dotfiles', "$ENV{HOME}/.dotfiles");
 }
 
 opendir(my $dh, $ENV{'HOME'}) or die "Cannot open directory: $!";
 
 print "Symlinking Dotfiles...\n";
-system('ln', '-sf', "$ENV{'HOME'}/.dotfiles/.*", '.');
+system('ln', '-sf', glob("$ENV{HOME}/.dotfiles/.*"), '.');
 
-chdir $dh;
 print "Setting up default shell...\n";
-if (-d "$ENV{'HOME'}/.fizsh") {
-    chdir "$ENV{'HOME'}/.fizsh";
+if (-d "$ENV{HOME}/.fizsh") {
+    chdir "$ENV{HOME}/.fizsh";
     system('./configure');
     system('make');
     system('doas', 'make', 'install');
-    system('cp', "$ENV{'HOME'}/.dotfiles/.fizshrc", "$ENV{'HOME'}/.fizsh/.fizshrc");
+    system('cp', "$ENV{HOME}/.dotfiles/.fizshrc", "$ENV{HOME}/.fizsh/.fizshrc");
     system('chsh', '-s', '/usr/local/bin/fizsh');
 } else {
-    system('git', 'clone', 'https://github.com/zsh-users/fizsh.git', "$ENV{'HOME'}/.fizsh");
-    chdir "$ENV{'HOME'}/.fizsh";
+    system('git', 'clone', 'https://github.com/zsh-users/fizsh.git', "$ENV{HOME}/.fizsh");
+    chdir "$ENV{HOME}/.fizsh";
     system('./configure');
     system('make');
     system('doas', 'make', 'install');
-    system('cp', "$ENV{'HOME'}/.dotfiles/.fizshrc", "$ENV{'HOME'}/.fizsh/.fizshrc");
+    system('cp', "$ENV{HOME}/.dotfiles/.fizshrc", "$ENV{HOME}/.fizsh/.fizshrc");
     system('chsh', '-s', '/usr/local/bin/fizsh');
 }
 
-
 print "Installing backgrounds...\n";
 system('doas', 'mkdir', '-p', '/usr/local/share/backgrounds');
-system('doas', 'cp', '-rvf', "$ENV{'HOME'}/.dotfiles/backgrounds/*", '/usr/local/share/backgrounds');
-
+system('doas', 'cp', '-rvf', glob("$ENV{HOME}/.dotfiles/backgrounds/*"), '/usr/local/share/backgrounds');
 
 print "Setting up Xenodm...\n";
-system('doas', 'ln', '-sf', "$ENV{'HOME'}/.dotfiles/xenodm_config/*", '/etc/X11/xenodm/');
-
+system('doas', 'ln', '-sf', glob("$ENV{HOME}/.dotfiles/xenodm_config/*"), '/etc/X11/xenodm/');
 
 print "Installing Fonts...\n";
-system('doas', 'cp', '-rvf', "$ENV{'HOME'}/.dotfiles/.fonts/*", '/usr/X11R6/lib/X11/fonts/TTF/');
+system('doas', 'cp', '-rvf', glob("$ENV{HOME}/.dotfiles/.fonts/*"), '/usr/X11R6/lib/X11/fonts/TTF/');
 system('doas', 'fc-cache', '-fv');
-
 
 print "Compiling in rust programs...( this is gonna take a bit :3 )\n";
 our @rsdeps = ('fd-find', 'sd', 'onefetch', 'tokei', 'zoxide', 'broot', 'du-dust', 'cargo-update-installed');
 system('cargo', 'install', @rsdeps);
 
-
 print "Compiling in GNU shuf re-implementation...\n";
-system('git', 'clone', 'https://github.com/ibara/shuf.git', "$ENV{'HOME'}/.shuf");
-chdir "$ENV{'HOME'}/.shuf";
+system('git', 'clone', 'https://github.com/ibara/shuf.git', "$ENV{HOME}/.shuf");
+chdir "$ENV{HOME}/.shuf";
 system('./configure');
 system('make');
 system('doas', 'make', 'install');
 
-
 print "Compiling in suckless-term...\n";
-system('git', 'clone', 'https://github.com/izder456/st.git', "$ENV{'HOME'}/.st");
-chdir "$ENV{'HOME'}/.st";
+system('git', 'clone', 'https://github.com/izder456/st.git', "$ENV{HOME}/.st");
+chdir "$ENV{HOME}/.st";
 system('make');
 system('doas', 'make', 'install');
-
 
 print "Compiling in afetch...\n";
-system('git', 'clone', 'https://github.com/13-CF/afetch.git', "$ENV{'HOME'}/.afetch");
-chdir "$ENV{'HOME'}/.afetch";
+system('git', 'clone', 'https://github.com/13-CF/afetch.git', "$ENV{HOME}/.afetch");
+chdir "$ENV{HOME}/.afetch";
 system('make');
 system('doas', 'make', 'install');
-
 
 closedir $dh;
 
