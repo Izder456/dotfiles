077c75054f2c271aed370ac8dcfa9f2b61350ef4
izder456 <izder456@disroot.org>
2023-10-29 17:07:52 -0500

    update

---
+++ quicklisp-setup.lisp
@@ -0,0 +1,7 @@
+(quicklisp-quickstart:install :path "~/.quicklisp")
+(ql:add-to-init-file)
+(ql:quickload '("clx"
+                "cl-ppcre"
+                "alexandria"
+                "cl-fad"
+                "anaphora"))
