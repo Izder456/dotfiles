1d9e8488fcbf6d10e5b3c9b61c526168b96edbef
izder456 <izder456@disroot.org>
2024-02-06 07:34:05 -0600

    add bgs, use new quicklisp dist

--- quicklisp-setup.lisp
+++ quicklisp-setup.lisp
@@ -1,5 +1,7 @@
 (quicklisp-quickstart:install :path "~/.quicklisp")
 (ql:add-to-init-file)
+(ql-dist:install-dist "http://dist.ultralisp.org/"
+                      :prompt nil)
 (ql:quickload '("clx"
                 "cl-ppcre"
                 "alexandria"
