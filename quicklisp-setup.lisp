77db58413d59cc728b3ff99ac95086916d90d74e
izder456 <izder456@disroot.org>
2024-02-13 16:08:07 -0600

    optimize

--- quicklisp-setup.lisp
+++ quicklisp-setup.lisp
@@ -1,11 +1,12 @@
 (quicklisp-quickstart:install :path "~/.quicklisp")
 (ql:add-to-init-file)
 (ql-dist:install-dist "http://dist.ultralisp.org/"
-                      :prompt nil)
+		      :prompt nil)
 (ql:quickload '("clx"
-                "cl-ppcre"
-                "alexandria"
-                "cl-fad"
-                "anaphora"
-                "drakma"
-                "slynk"))
+		"cl-ppcre"
+		"alexandria"
+		"cl-fad"
+		"xembed"
+		"anaphora"
+		"drakma"
+		"slynk"))
